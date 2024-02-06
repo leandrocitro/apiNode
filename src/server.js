cec85e915bfe6e17653783a50896049eb8c4d9b4
leandro.citrodonegit config --global user.name leandro.citro <leandro_citro@hotmail.com>
2024-02-06 01:54:27 -0300

    criando rotas get

--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
-const express = require('express');
+const express = require('express'); //Importação do Express
 
-const app = express();
+const app = express(); //Inicialização do Express
 
-const PORT = 3333;
-app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
+app.get("/message/:id/:user", (request, response) => {
+const { id, user } = request.params;
+
+    response.send(`
+    Id da mensagem: ${id}.
+    Para o usuário: ${user}.
+
+`);
+});
+
+const PORT = 3333; //Criação de uma constante para definir a porta 3333
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`)); 
