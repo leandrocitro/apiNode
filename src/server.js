310d0e17cec557ec3307c0144b05167ed925460c
leandro.citrodonegit config --global user.name leandro.citro <leandro_citro@hotmail.com>
2024-02-06 12:00:20 -0300

    Estruturando projeto com Routes e controllers

--- src/server.js
+++ src/server.js
@@ -1,23 +1,11 @@
 const express = require('express'); //Importação do Express
 
+const routes = require("./routes/")
+
 const app = express(); //Inicialização do Express
 app.use(express.json());
 
- /* app.get("/message/:id/:user", (request, response) => { //Request Params
-const { id, user } = request.params; 
-
-    response.send(`
-    Id da mensagem: ${id}.
-    Para o usuário: ${user}.
-
-`);
-}); */
-
-app.post("/users", (request, response) => {
-    const { name, email, password } = request.body;
-        
-    response.send(`Usuário: ${name} - email: ${email} - password: ${password}`);
-})
+app.use(routes);
 
 const PORT = 3333; //Criação de uma constante para definir a porta 3333
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`)); 
