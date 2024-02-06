77bf5bc4c0ac7cd729078a122331c3c7a277ed4b
leandro.citrodonegit config --global user.name leandro.citro <leandro_citro@hotmail.com>
2024-02-06 03:02:55 -0300

    Exemplos de métodos Get e Post

--- src/server.js
+++ src/server.js
@@ -1,16 +1,23 @@
 const express = require('express'); //Importação do Express
 
 const app = express(); //Inicialização do Express
+app.use(express.json());
 
-app.get("/message/:id/:user", (request, response) => {
-const { id, user } = request.params;
+ /* app.get("/message/:id/:user", (request, response) => { //Request Params
+const { id, user } = request.params; 
 
     response.send(`
     Id da mensagem: ${id}.
     Para o usuário: ${user}.
 
 `);
-});
+}); */
+
+app.post("/users", (request, response) => {
+    const { name, email, password } = request.body;
+        
+    response.send(`Usuário: ${name} - email: ${email} - password: ${password}`);
+})
 
 const PORT = 3333; //Criação de uma constante para definir a porta 3333
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`)); 
