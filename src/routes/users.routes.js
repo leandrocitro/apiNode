dfba347a29a8792a850963c7a63eb6dee42b64a8
leandro.citrodonegit config --global user.name leandro.citro <leandro_citro@hotmail.com>
2024-02-07 00:28:15 -0300

    Estruturando projeto

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,17 +5,21 @@ const UsersController = require("../controllers/UsersController");
 const usersRoutes = Router();
 
 
-function myMiddleware(request, response, next) {
-    console.log("Você passou pelo middleware")
+function myMiddleware(request, response, next) { //Criando o Middleware
+    console.log("Você passou pelo middleware");
+    
+
+    if (!request.body.isAdmin) {
+        return response.json({ message: "User unautorized" });
+    } 
 
     next();
+
 }
-
-
 
 
 const usersController = new UsersController;
 
-usersRoutes.post("/", myMiddleware, usersController.create);
+usersRoutes.post("/", myMiddleware, usersController.create); //Utilizando o Middleware
 
 module.exports = usersRoutes;
