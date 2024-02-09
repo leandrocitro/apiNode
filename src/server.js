4ad53d1c4e8f09a5d852530e82d051e73d9229be
leandro.citrodonegit config --global user.name leandro.citro <leandro_citro@hotmail.com>
2024-02-08 23:57:24 -0300

    Estruturando projeto

--- src/server.js
+++ src/server.js
@@ -7,5 +7,5 @@ app.use(express.json());
 
 app.use(routes);
 
-const PORT = 3333; //Criação de uma constante para definir a porta 3333
+const PORT = 3300; //Criação de uma constante para definir a porta 3333
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`)); 
