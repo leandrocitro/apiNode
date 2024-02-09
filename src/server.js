89a2c86d9b056a92c6cb9f1603ae937847207573
leandro.citrodonegit config --global user.name leandro.citro <leandro_citro@hotmail.com>
2024-02-09 00:38:39 -0300

    Tratamento de errors

--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
+require("express-async-errors");
+
+const AppError = require("./utils/AppError");
 const express = require('express'); //Importação do Express
 
 const routes = require("./routes/")
@@ -7,5 +10,21 @@ app.use(express.json());
 
 app.use(routes);
 
-const PORT = 3300; //Criação de uma constante para definir a porta 3333
-app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`)); 
+app.use(( error, request, response, next ) => {
+    if(error instanceof AppError) {
+        return response.status(error.statusCode).json({
+            status: "error",
+            message: error.message
+        });
+    }
+
+    console.error(error);
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal server error",
+    });
+});
+
+const PORT = 3333; //Criação de uma constante para definir a porta 3333
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
