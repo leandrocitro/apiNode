e907d45afb32080d4f8fea4b7809ae7cd1d18148
leandro.citrodonegit config --global user.name leandro.citro <leandro_citro@hotmail.com>
2024-02-11 13:57:02 -0300

    Configurando BD

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 require("express-async-errors");
 
+const database = require("./database/sqlite");
+
 const AppError = require("./utils/AppError");
 const express = require('express'); //Importação do Express
 
@@ -9,6 +11,8 @@ const app = express(); //Inicialização do Express
 app.use(express.json());
 
 app.use(routes);
+
+database();
 
 app.use(( error, request, response, next ) => {
     if(error instanceof AppError) {
