2f117ca1f265bec9a2362b49401b750df78d013d
leandro.citrodonegit config --global user.name leandro.citro <leandro_citro@hotmail.com>
2024-02-12 20:33:46 -0300

    Criando tabelas com migrations utilizando knex

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,17 @@
 const path = require("path");
 
-
 module.exports = {
 
   development: {
     client: 'sqlite3',
     connection: {
-      filename: path.reolve(__dirname, "src", "datebase", "database.db")
-    
-  }, 
-  useNullAsDefault:true;
-}
+      filename: path.resolve(__dirname, "src", "database", "database.db")
+
+    },
+    migrations: {
+      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
+    },
+    useNullAsDefault: true
+  },
 
 };
