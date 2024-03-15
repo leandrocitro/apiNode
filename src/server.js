81d17d51615e92af18c200938ce8da212fb5166c
leandro.citrodonegit config --global user.name leandro.citro <leandro_citro@hotmail.com>
2024-03-14 23:48:52 -0300

    Enviando form SignUp com Axios

--- src/server.js
+++ src/server.js
@@ -3,12 +3,14 @@ const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
 const uploadConfig = require("./configs/upload");
 
+const cors = require("cors");
 const express = require('express'); //Importação do Express
 const routes = require("./routes/");
 
 migrationsRun();
 
 const app = express(); //Inicialização do Express
+app.use(cors());
 app.use(express.json());
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
